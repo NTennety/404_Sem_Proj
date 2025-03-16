13cf7fc540b2df0d2ec89f8da7dab3e958152554
Narayana Tennety <Naray@Nabis-AIr.local>
2025-03-16 12:55:32 -0400

    Added Pixels

--- CXR8/Labels/pixelizing.py
+++ CXR8/Labels/pixelizing.py
@@ -28,6 +28,24 @@ diseased_pixels_array = np.array(diseased_pixels)
 
 np.save('diseased_pixels.npy', diseased_pixels_array)
 print("Data Saved")
+
+control_pixels = []
+
+for index, row in control_data.iterrows():
+    images_file_name = row["Image Index"]
+    
+    image_path = os.path.join(images_folder,images_file_name)
+    if os.path.exists(image_path):
+        image = cv2.imread(image_path)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        diseased_pixels.append(gray_image)
+    else:
+        print(f"Image file not found: {image_path}")
+
+control_pixels_array = np.array(diseased_pixels)
+
+np.save('control_pixels.npy', control_pixels_array)
+print("Data Saved")
 # for index , row in disease_data.iterrows(): 
 
 #     if index >= 50:
