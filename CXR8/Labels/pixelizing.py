4e95e0edb70f56b3df6a78d01fbf7b80b3ad08c1
Narayana Moorthy Tennety Ganesh Moorthy <tennetyn@dev-intel16-k80.i>
2025-03-16 12:09:01 -0400

    Added Cleaning

---
+++ CXR8/Labels/pixelizing.py
@@ -0,0 +1,45 @@
+import os
+import tensorflow as tf
+import pandas as pd
+import cv2
+import numpy as np
+
+images_folder = "../../../images"
+
+data = pd.DataFrame(pd.read_csv("Data_Entry_2017_v2020.csv"))
+
+disease_data = data[data["Finding Labels"] != "No Finding"]
+control_data = data[data["Finding Labels"] == "No Finding"]
+
+diseased_pixels = []
+
+for index, row in disease_data.iterrows():
+    if index >= 50:
+        break
+
+    images_file_name = row["Image Index"]
+    
+    image_path = os.path.join(images_folder,images_file_name)
+    if os.path.exists(image_path):
+    	print("!")
+# for index , row in disease_data.iterrows(): 
+
+#     if index >= 50:
+#         break
+
+#     images_file_name = row['Image Index']
+
+#     image_path = os.path.join(images_folder,images_file_name)
+
+#     if os.path.exists(image_path):
+#         image = cv2.imread(image_path)
+#         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#         diseased_pixels.append(gray_image)
+#     else:
+#         print(f"Image file not found: {image_path}")
+    
+
+# diseased_pixels_array = np.array(diseased_pixels)
+
+# np.save('diseased_pixels.npy', diseased_pixels_array)
+# print("Data Saved")
